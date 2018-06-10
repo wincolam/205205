d06dae89923702cad0daeb6ef6b61188c94b155a
Leo Sin <lam.winco9@gmail.com>
2018-06-11 05:23:03 +0800

    updated

--- formexample.py
+++ formexample.py
@@ -16,87 +16,96 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisupposedtobesecret!'
 Bootstrap(app)
 
-db = pymysql.connect( host="localhost", user="user", password="123456", database="205CDE")
-cursor = db.cursor()
-sqlloginform = """CREATE TABLE loginform (email varchar(80) primary key, username varchar(15), password varchar(80))"""
-cursor.execute("DROP TABLE IF EXISTS loginform")
-cursor.execute(sqlloginform)
-db.close()
+def connection():
+    return pymysql.connect( host="localhost", user="user", password="123456", database="205CDE")
 
+def select(sql):
+    print (sql)
+    conn = connection()
+    with conn.cursor() as cur:
+        cur.execute(sql)
+        return cur.fetchall()
+
+
+    
+
+# cursor = db.cursor()
+# cursor.execute("DROP TABLE IF EXISTS loginform")
+# sqlloginform = """CREATE TABLE loginform (email varchar(80) primary key, username varchar(15), password varchar(80))"""
+# cursor.execute(sqlloginform)
+# db.close()
 
 
 @app.route("/", defaults={"filename": "index.html"})
 @app.route("/<path:filename>", methods = ["GET", "POST"])
 def display(filename):
-	try:
-		return render_template(filename)
-	except TemplateNotFound:
-		return app.send_static_file(filename)
-
-class LoginForm(Form):
-	propertyname = StringField("username", validators=[InputRequired(), Length(min=4, max=15)])
-	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-	remember = BooleanField('remember me')
-	email = TextField("Email",[validators.Required("Please enter your email address.")])
-	submit = SubmitField("Sign in")
+    try:
+        return render_template(filename)
+    except TemplateNotFound:
+        return app.send_static_file(filename)
 
 class add(Form):
-	name=TextField("Name of User",[validators.Required("Please enter your name.")])
-	price=TextField("Name of User",[validators.Required("Please enter your name.")])
-	interval=TextField("Name of User",[validators.Required("Please enter your name.")])
-	age=TextField("Name of User",[validators.Required("Please enter your name.")])
-	projecttype=TextField("Name of User",[validators.Required("Please enter your name.")])
-	developer=TextField("Name of User",[validators.Required("Please enter your name.")])
-	contactname=TextField("Name of User",[validators.Required("Please enter your name.")])
-	contactnumber=TextField("Name of User",[validators.Required("Please enter your name.")])
+    name=TextField("Name of User",[validators.Required("Please enter your name.")])
+    price=TextField("Name of User",[validators.Required("Please enter your name.")])
+    interval=TextField("Name of User",[validators.Required("Please enter your name.")])
+    age=TextField("Name of User",[validators.Required("Please enter your name.")])
+    projecttype=TextField("Name of User",[validators.Required("Please enter your name.")])
+    developer=TextField("Name of User",[validators.Required("Please enter your name.")])
+    contactname=TextField("Name of User",[validators.Required("Please enter your name.")])
+    contactnumber=TextField("Name of User",[validators.Required("Please enter your name.")])
 
 class RegisterForm(Form):
-	username = TextField("Name of User",[validators.Required("Please enter your name.")])
-	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-	email = TextField("Email",[validators.Required("Please enter your email address.")])
-	submit = SubmitField("submit")
+    username = TextField("Name of User",[validators.Required("Please enter your name.")])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    email = TextField("Email",[validators.Required("Please enter your email address.")])
+    submit = SubmitField("submit")
 
 
 
-@app.route('/login')
-def login():
-	form = LoginForm()
-
-	return render_template('login.html', form=form)
-	
-	
+@app.route('/login', methods=["POST", "GET"])
+def login(errmsg=""):
+    if request.method == "POST":
+        sql = "SELECT * FROM user WHERE username='%s' AND password='%s'" % (request.form['username'], request.form['password'])
+        result = select(sql)
+        if len(result) == 1:
+            session['username'] = request.form['username']
+            return redirect("/")
+        return redirect(url_for('login'), errmsg="incorrect username / password")
+        
+    return render_template('login.html')
+    
+    
 
 @app.route('/signup' , methods = ['GET', 'POST'])
 def signup():
-	form = RegisterForm(request.form)
-		 
-	if request.method == "POST":			
-		username=request.form['username']
-		password=request.form['password']
-		email=request.form['email']
-		db = pymysql.connect( host="localhost", user="root", password="123456", database="205CDE")
-		cursor = db.cursor()
-		
-		sql = "INSERT INTO loginform (username, password, email) VALUES ('%s', '%s', '%s')" % (username, password, email)
-		print(sql)
-		cursor.execute(sql)
-		db.commit()
-		flash("Thanks for sign up")
+    form = RegisterForm(request.form)
+         
+    if request.method == "POST":            
+        username=request.form['username']
+        password=request.form['password']
+        email=request.form['email']
+        db = pymysql.connect( host="localhost", user="root", password="123456", database="205CDE")
+        cursor = db.cursor()
+        
+        sql = "INSERT INTO loginform (username, password, email) VALUES ('%s', '%s', '%s')" % (username, password, email)
+        cursor.execute(sql)
+        db.commit()
+        flash("Thanks for sign up")
 
-		cursor.close()
-		db.close()
-		gc.collect()
+        cursor.close()
+        db.close()
+        gc.collect()
 
-		session['logged_in'] = True
-		session['username'] = username
+        session['logged_in'] = True
+        session['username'] = username
 
-		return redirect(url_for('add'))
+        return redirect(url_for('add'))
 
-	return render_template('signup2.html', form = form)		 
+    return render_template('signup2.html', form = form)      
 
 @app.route('/add')
 def add():
     return render_template('add.html', form=form)
   
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run(debug = True)
